e966254cad3bd1060bc3a36c2ccd6e4555aa7c35
KRYG <abcdef0509a@gmail.com>
2025-06-03 14:08:10 +0900

    [add]アニメーションにFinalIKを導入しバットをスイングしたときの位置がカーソルの位置になるように実装

--- Assets/Sqript/BatterAnimationControl.cs
+++ Assets/Sqript/BatterAnimationControl.cs
@@ -1,3 +1,4 @@
+using RootMotion.FinalIK;
 using System.Collections;
 using UnityEngine;
 
@@ -7,26 +8,39 @@ public class BatterAnimationControl : MonoBehaviour
 
     [SerializeField] Batting _batting;
 
+    [SerializeField] AimIK _aimIK;
+
+    [SerializeField] CursorController _cursorController;
+
     private Quaternion _batterStartQuaternion;
     private Vector3 _batterStartPosition;
+
+
 
     private void Start()
     {
         _batterStartQuaternion = transform.rotation;
         _batterStartPosition = transform.position;
+        _aimIK.enabled = false;
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _cursorController.IsCursorInZone)
         {
             _batterAnim.SetTrigger("Swing");
             StartCoroutine(PositionReset());
         }
     }
 
+    public void AimIKCall()
+    {
+        _aimIK.enabled = true;
+    }
+
     public void BattingBallCall()
     {
         _batting.BattingBall();
+        _aimIK.enabled = false;
     }
 
     IEnumerator PositionReset()
