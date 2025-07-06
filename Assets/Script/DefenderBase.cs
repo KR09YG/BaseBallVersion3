3fda552191687a54474d54fcd23d3907e3731561
KRYG <abcdef0509a@gmail.com>
2025-07-07 01:06:49 +0900

    [add]Defender（守備）のベースクラスを作成

---
+++ Assets/Script/DefenderBase.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+public abstract class DefenderBase : MonoBehaviour
+{
+    [Header("パラメータ設定")]
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _dashSpeed;
+    [SerializeField] private float _chachRange;
+    [SerializeField] private float _chachHeight;
+    [SerializeField] private float _reactionSpeed;
+
+    [Header("投球能力設定")]
+    [SerializeField] private float _throwSpeed;
+    [SerializeField] private Transform _throwPoint;
+
+    protected Rigidbody _ballRb;
+    protected Batting _batting;
+    protected DefenseState _currentState = DefenseState.Ready;
+    protected GameObject _ball;
+    protected Vector3 _targetPosition;
+    protected bool _isChasing = false;
+    protected float _reactionTimer = 0f;
+    protected BaseManager _baseManager;
+
+    protected enum DefenseState
+    {
+        Ready, Chasing, Catching, Throwing, Returning
+    }
+
+    protected virtual void Start()
+    {
+        _currentState = DefenseState.Ready;
+    }
+
+    protected virtual void Update()
+    {
+        switch (_currentState)
+        {
+            case DefenseState.Ready:
+                HandleReadyState();
+                break;
+            case DefenseState.Chasing:
+                HandleChasingState();
+                break;
+            case DefenseState.Catching:
+                HandleCatchingState();
+                break;
+            case DefenseState.Throwing:
+                HandleThrowingState();
+                break;
+            case DefenseState.Returning:
+                HandleReturningState();
+                break;
+        }
+    }
+
+    protected virtual void HandleReadyState()
+    {
+        if (_batting.IsHit)
+        {
+            _reactionTimer += Time.deltaTime;
+            if (_reactionSpeed < _reactionTimer && IsChaseable())
+            {
+                _currentState = DefenseState.Chasing;
+                _reactionTimer = 0f;
+            }
+        }
+    }
+    protected virtual void HandleChasingState()
+    {
+
+    }
+
+    protected virtual void HandleCatchingState()
+    {
+    
+    }
+
+    protected virtual void HandleThrowingState()
+    {
+
+    }
+
+    protected virtual void HandleReturningState()
+    {
+        
+    }
+    protected abstract bool IsChaseable();
+
+}
