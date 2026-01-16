758322b5fc2e28039a60a1541616364d9e9debef
KRYG <abcdef0509a@gmail.com>
2026-01-16 16:32:18 +0900

    [add]打球結果を受け取る → 誰が捕球もしくは打球を追いかけるのか決定までの流れを実装

--- Assets/Script/Defender/DefenseCalculator.cs
+++ Assets/Script/Defender/DefenseCalculator.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 public static class DefenseCalculator
 {
     public static CatchPlan CalculateCatchPlan(
-        List<Vector3> trajectory,
-        float totalFlightTime,
-        List<FielderController> fielders)
+    List<Vector3> trajectory,
+    float deltaTime,
+    List<FielderController> fielders)
     {
         CatchPlan bestPlan = new CatchPlan
         {
@@ -14,32 +14,65 @@ public static class DefenseCalculator
             CatchTime = float.MaxValue
         };
 
-        int count = trajectory.Count;
-
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < trajectory.Count; i++)
         {
-            float t = (float)i / (count - 1) * totalFlightTime;
+            // ボールがその座標に来る時間
+            float t = i * deltaTime;
             Vector3 ballPos = trajectory[i];
 
             foreach (var fielder in fielders)
             {
                 FielderData data = fielder.Data;
 
+                // 野手がボールに到達するのにかかる時間
                 float moveTime =
                     Vector3.Distance(fielder.transform.position, ballPos)
                     / data.MoveSpeed
                     + data.ReactionTime;
 
+                // ボールがその座標に来るまでに野手が到達できるか
                 if (moveTime <= t && t < bestPlan.CatchTime)
                 {
+                    // キャッチ可能な最速プランを更新
                     bestPlan.CanCatch = true;
                     bestPlan.Catcher = fielder;
                     bestPlan.CatchPoint = ballPos;
                     bestPlan.CatchTime = t;
                     bestPlan.CatchTrajectoryIndex = i;
+                    break;
                 }
             }
         }
+
+        // 終点までにキャッチできない場合の処理
+        if (!bestPlan.CanCatch)
+        {
+            // 最終地点に最も早く到達できる野手を探す
+            float bestArrivalTime = float.MaxValue;
+            Vector3 finalPos = trajectory[trajectory.Count - 1];
+
+            foreach (var fielder in fielders)
+            {
+                FielderData data = fielder.Data;
+
+                float moveTime =
+                    Vector3.Distance(fielder.transform.position, finalPos)
+                    / data.MoveSpeed
+                    + data.ReactionTime;
+
+                if (moveTime < bestArrivalTime)
+                {
+                    bestArrivalTime = moveTime;
+
+                    bestPlan.Catcher = fielder;
+                    bestPlan.CatchPoint = finalPos;
+                    bestPlan.CatchTime = moveTime;
+                    bestPlan.CatchTrajectoryIndex = trajectory.Count - 1;
+                }
+            }
+        }
+
+        Debug.Log(bestPlan.Catcher);
 
         return bestPlan;
     }
--- Assets/Script/Defender/DefenseManager.cs
+++ Assets/Script/Defender/DefenseManager.cs
@@ -4,28 +4,65 @@ using System.Collections.Generic;
 public class DefenseManager : MonoBehaviour
 {
     [SerializeField] private List<FielderController> _fielders;
+    [SerializeField] private BattingResultEvent _battingResultEvent;
+    [SerializeField] private BattingBallTrajectoryEvent _battingBallTrajectoryEvent;
 
-    public void OnBallHit(
-        List<Vector3> trajectory,
-        float totalFlightTime)
+    private BattingBallResult _result;
+    private List<Vector3> _trajectory;
+
+    private const float DELTA_TIME = 0.01f;
+
+    private void OnEnable()
+    {
+        _battingResultEvent.RegisterListener(OnBattingResult);
+        _battingBallTrajectoryEvent.RegisterListener(OnBattingBallTrajectory);
+    }
+
+    private void OnDisable()
+    {
+        _battingResultEvent.UnregisterListener(OnBattingResult);
+        _battingBallTrajectoryEvent.UnregisterListener(OnBattingBallTrajectory);
+    }
+
+
+    private void OnBattingBallTrajectory(List<Vector3> trajectory)
+    {
+        _trajectory = trajectory;
+    }
+
+    private void OnBattingResult(BattingBallResult result)
+    {
+        _result = result;
+
+        if (_trajectory != null)
+        {
+            OnBallHit(_trajectory, result);
+            _trajectory = null;
+        }
+    }
+
+    public void OnBallHit(List<Vector3> trajectory, BattingBallResult result)
     {
         CatchPlan catchPlan =
             DefenseCalculator.CalculateCatchPlan(
                 trajectory,
-                totalFlightTime,
+                DELTA_TIME,
                 _fielders);
 
-        DefenseRoles roles =
-            DefenseRolePlanner.CreateRoles(
-                catchPlan,
-                _fielders);
+        Debug.Log($"CanCatch: {catchPlan.CanCatch}");
+        Debug.Log($"Catcher: {catchPlan.Catcher.Data.Position}");
+        Debug.Log($"CatchPoint: {catchPlan.CatchPoint}");
+        Debug.Log($"CatchTime: {catchPlan.CatchTime}");
 
-        IssueOrders(catchPlan, roles);
+        //DefenseRoles roles =
+        //    DefenseRolePlanner.CreateRoles(
+        //        catchPlan,
+        //        _fielders);
+
+        //IssueOrders(catchPlan, roles);
     }
 
-    private void IssueOrders(
-        CatchPlan catchPlan,
-        DefenseRoles roles)
+    private void IssueOrders(CatchPlan catchPlan, DefenseRoles roles)
     {
         if (catchPlan.CanCatch)
         {
